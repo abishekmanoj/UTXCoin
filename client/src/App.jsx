80715d054180d49255db20fab5ad5e16d68637e5
Abishek Manoj <abishekmanojx@gmail.com>
2025-12-17 10:03:55 +0400

    Added footer to UI

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar"
+import Footer from './components/Footer.jsx'
 
 import Home from "./pages/Home.jsx";
 import BlockDetails from "./pages/BlockDetails.jsx";
@@ -11,17 +12,17 @@ import Blockchain from "./pages/Blockchain.jsx";
 export default function App() {
     return (
        <div className="bg-gray-950 min-h-screen text-white">
-             <Navbar />
-             <div className="p-6">
-               <Routes>
+            <Navbar />
+              <div className="p-6">
+                <Routes>
                     <Route path="/" element={<Home />} />
                     <Route path="/blocks" element={<Blockchain />} />
                     <Route path="/block/:id" element={<BlockDetails />} />
                     <Route path="/pool" element={<Pool />} />
                     <Route path="/wallet" element={<Wallet />} />
-               </Routes>
-             </div>
-
+                </Routes>
+              </div>
+            <Footer />
         </div>
     );
 }
---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,10 @@
+export default function Footer() {
+  return (
+    <footer className="fixed bottom-4 right-4 text-sm text-gray-400">
+      <span>
+        Created by{" "}
+        <span className="text-blue-400 font-semibold">Abishek Manoj</span>
+      </span>
+    </footer>
+  );
+}
